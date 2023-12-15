8189c6aedc143d73a03d8c04093574850fbaf329
Thada Wangthammang <thada.wth@gmail.com>
2023-12-15 17:50:21 +0700

    feat: when data source change back to update consumer (actiontable)

--- automated-google-sheet/src/hooks/ensureColumnCorrect.ts
+++ automated-google-sheet/src/hooks/ensureColumnCorrect.ts
@@ -59,6 +59,65 @@ function ensureColumnCorrect(
 
 function registerEnsureColumnCorrect(e: GoogleAppsScript.Events.SheetsOnEdit, hook: EnsureColumnCorrectHookOption) {
   logger(`Registering ensure column correct on At=${e.range.getA1Notation()}, ${JSON.stringify(hook)}`);
+  handleWhenSelectActionTable(e, hook);
+  handleWhenDataSourcesChange(e, hook);
+}
+
+function handleWhenDataSourcesChange(e: GoogleAppsScript.Events.SheetsOnEdit, hook: EnsureColumnCorrectHookOption) {
+  const changedSheet = e.range.getSheet();
+  if (
+    !(
+      e.range.getColumn() == hook.source.checkColumn &&
+      e.value != '' &&
+      changedSheet.getSheetId() == hook.sourceSheetId
+    )
+  ) {
+    // Check if the edited cell is in the specified target column and is not empty
+    return;
+  }
+
+  const sourceIdColumnData: string[] = (
+    changedSheet
+      .getRange(hook.source.startRow, hook.source.idColumn, changedSheet.getLastRow(), 1)
+      .getValues() as string[][]
+  ).map(row => row[0]);
+  const sourceCheckColumnData: string[] = (
+    changedSheet
+      .getRange(hook.source.startRow, hook.source.checkColumn, changedSheet.getLastRow(), 1)
+      .getValues() as string[][]
+  ).map(row => row[0]);
+
+  let sheet = e.source.getSheetByName(hook.sheet.name);
+  invariant(sheet, `Sheet '${hook.sheet.name}' not found`);
+  const sourceId = sourceIdColumnData[sourceCheckColumnData.indexOf(e.value)];
+  addDataToMultipleRows(hook, e.value, sourceId);
+}
+
+function addDataToMultipleRows(
+  hook: EnsureColumnCorrectHookOption,
+  data: string,
+  sourceId: string
+) {
+  const sheet = getSheetById(hook.sheet.id);
+  invariant(sheet, `Sheet '${hook.sheet.name}' not found`);
+
+  var lastRow = sheet.getLastRow();
+  var range = sheet.getRange(hook.startRow, hook.checkColumn, lastRow - 1); // Start from the `startRow` row
+  var uuidRange = sheet.getRange(hook.startRow, hook.idColumn, lastRow - 1);
+
+  var targetValues = range.getValues();
+  var uuidValues = uuidRange.getValues();
+
+  for (var i = 0; i < targetValues.length; i++) {
+    if (uuidValues[i][0] === sourceId) {
+      // If the target cell is empty and the UUID cell is empty, add a UUID to the UUID column
+      sheet.getRange(i + hook.startRow, hook.checkColumn).setValue(data);
+      logger(`Added '${data}' to row ${i + hook.startRow}`);
+    }
+  }
+}
+
+function handleWhenSelectActionTable(e: GoogleAppsScript.Events.SheetsOnEdit, hook: EnsureColumnCorrectHookOption) {
   const sourceSheet = getSheetById(hook.sourceSheetId);
   invariant(sourceSheet, `Source sheet not found. ID: ${hook.sourceSheetId}`);
 
@@ -74,13 +133,10 @@ function registerEnsureColumnCorrect(e: GoogleAppsScript.Events.SheetsOnEdit, ho
   ).map(row => row[0]);
 
   if (e.range.getColumn() == hook.checkColumn && e.value != '') {
-    logger(`In range, changed range: ${e.range.getA1Notation()}`);
     let sheet = e.source.getSheetByName(hook.sheet.name);
     invariant(sheet, `Sheet '${hook.sheet.name}' not found`);
     const sourceId = sourceIdColumnData[sourceCheckColumnData.indexOf(e.value)];
     addDataToRow(sheet, hook, sourceId, e.range.getRow());
-  } else {
-    logger(`Out of range, changed range: ${e.range.getA1Notation()}`);
   }
 }
 
