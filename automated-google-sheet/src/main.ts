4f62d0438562d4adc430064bae6da485d0adb7f9
Thada Wangthammang <thada.wth@gmail.com>
2024-04-08 10:40:47 +0700

    disable onEdit SimpleTrigger

--- automated-google-sheet/src/main.ts
+++ automated-google-sheet/src/main.ts
@@ -4,10 +4,10 @@
  * @see https://developers.google.com/apps-script/guides/triggers#onedite
  * https://developers.google.com/apps-script/guides/triggers/events#edit
  */
-function onEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
+// function onEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
   /**
    *  Register manual trigger
    * https://developers.google.com/apps-script/guides/triggers/installable#manage_triggers_manually
    */
   // registerHooks(e);
-}
+// }
